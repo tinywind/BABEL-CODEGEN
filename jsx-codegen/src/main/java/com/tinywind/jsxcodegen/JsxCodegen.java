e252de82572636143973ca154489450299060912
Jeon JaeHyeong <tinywind@linkbiz.kr>
2016-06-24 15:59:15 +0900

    init commit

---
+++ jsx-codegen/src/main/java/com/tinywind/jsxcodegen/JsxCodegen.java
@@ -0,0 +1,45 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016 Jeon Jae-Hyeong
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.tinywind.jsxcodegen;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.InputStreamReader;
+
+/**
+ * @author tinywind
+ */
+public class JsxCodegen {
+    private final ScriptEngine engine;
+
+    public JsxCodegen() throws ScriptException {
+        engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("/asset/babel.min.js")));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
