09a5008d6fe820e5337b43dee8d9c91a36a83d2f
tinywind <tinywind0@gmail.com>
2016-06-25 13:02:08 +0900

    transform jsx -> js

--- babel-codegen-maven/src/main/java/com/tinywind/babelcodegen/maven/Plugin.java
+++ babel-codegen-maven/src/main/java/com/tinywind/babelcodegen/maven/Plugin.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  * <p>
- * Copyright (c) 2016 Jeon Jae-Hyeong
+ * Copyright (c) 2016 Jeon JaeHyeong
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
