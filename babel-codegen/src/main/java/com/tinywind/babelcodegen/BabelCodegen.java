19137174936c6cf78e562602815c1e6cbaa88c26
tinywind <tinywind0@gmail.com>
2016-06-25 13:02:08 +0900

    transform jsx -> js

--- babel-codegen/src/main/java/com/tinywind/babelcodegen/BabelCodegen.java
+++ babel-codegen/src/main/java/com/tinywind/babelcodegen/BabelCodegen.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  * <p>
- * Copyright (c) 2016 Jeon Jae-Hyeong
+ * Copyright (c) 2016 Jeon JaeHyeong
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,22 +24,24 @@
 package com.tinywind.babelcodegen;
 
 import com.tinywind.babelcodegen.jaxb.BabelOptions;
+import com.tinywind.babelcodegen.jaxb.BabelPresets;
 import com.tinywind.babelcodegen.jaxb.Configuration;
 import com.tinywind.babelcodegen.jaxb.Source;
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
-import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.script.SimpleBindings;
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.SchemaFactory;
 import java.io.*;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static java.lang.System.exit;
 
 /**
  * @author tinywind
@@ -48,19 +50,72 @@ public class BabelCodegen {
     public final static String BABEL_CODEGEN_XSD = "babel-codegen-0.1.xsd";
     public final static String REPO_XSD_URL = "https://raw.githubusercontent.com/tinywind/BABEL-CODEGEN/master/babel-codegen/src/main/resources/xsd/";
     public final static String REPO_BABEL_CODEGEN_XSD = REPO_XSD_URL + BABEL_CODEGEN_XSD;
+    private final SimpleBindings bindings = new SimpleBindings();
     private final ScriptEngine engine;
-    private final Invocable invocable;
 
-    public BabelCodegen() throws ScriptException {
+    public BabelCodegen() throws ScriptException, IOException {
         engine = new ScriptEngineManager().getEngineByName("nashorn");
-        engine.eval(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("/asset/babel.min.js")));
-        invocable = (Invocable) engine;
+        engine.eval(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("asset/babel.min.js")), bindings);
+    }
+
+    private static boolean isCorrected(Configuration configuration) {
+        if (configuration.getSources() == null || configuration.getSources().size() == 0)
+            return false;
+
+        for (Source source : configuration.getSources()) {
+            final String sourceDir = source.getSourceDir();
+            if (sourceDir == null)
+                return false;
+        }
+
+        return true;
+    }
+
+    private static void setDefault(Configuration configuration) {
+        if (configuration.getSources() == null)
+            configuration.setSources(new ArrayList<>());
+
+        for (Source source : configuration.getSources()) {
+            final String sourceDir = source.getSourceDir();
+            source.setSourceDir(sourceDir == null ? "" : sourceDir.trim());
+
+            final String targetDir = source.getTargetDir();
+            source.setTargetDir(targetDir == null ? source.getSourceDir() : targetDir.trim());
+
+            final String sourceEncoding = source.getSourceEncoding();
+            source.setSourceEncoding(sourceEncoding == null ? "UTF-8" : sourceEncoding.trim());
+
+            final String targetEncoding = source.getTargetEncoding();
+            source.setTargetEncoding(targetEncoding == null ? "UTF-8" : targetEncoding.trim());
+
+            final String filePostfix = source.getSourceFilePostfix();
+            source.setSourceFilePostfix(filePostfix == null ? ".jsx" : filePostfix.trim());
+
+            final String excludes = source.getExcludes();
+            source.setExcludes(excludes == null ? "" : excludes.trim());
+
+            final Boolean overwrite = source.isOverwrite();
+            source.setOverwrite(overwrite == null ? true : overwrite);
+
+            final Boolean recursive = source.isRecursive();
+            source.setOverwrite(recursive == null ? true : recursive);
+        }
+
+        if (configuration.getBabelOptions() == null)
+            configuration.setBabelOptions(new BabelOptions());
+        final BabelOptions babelOptions = configuration.getBabelOptions();
+
+        final BabelPresets presets = babelOptions.getPresets();
+        babelOptions.setPresets(presets == null ? BabelPresets.REACT : presets);
+
+        final Boolean minified = babelOptions.isMinified();
+        babelOptions.setMinified(minified == null ? false : minified);
     }
 
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("Usage : BabelCodegen <configuration-file>");
-            System.exit(-1);
+            exit(-1);
         }
 
         for (String arg : args) {
@@ -84,6 +139,12 @@ public class BabelCodegen {
                 System.out.println("Initialising properties: " + arg);
 
                 final Configuration configuration = load(in);
+                if (!isCorrected(configuration)) {
+                    System.out.println("Incorrect xml");
+                    System.exit(-1);
+                }
+
+                setDefault(configuration);
                 new BabelCodegen().generate(configuration);
             } catch (Exception e) {
                 System.err.println("Cannot read " + arg + ". Error : " + e.getMessage());
@@ -97,6 +158,7 @@ public class BabelCodegen {
                 }
             }
         }
+        System.out.println("complete transform JSX -> JS");
     }
 
     public static Configuration load(InputStream in) throws IOException {
@@ -108,6 +170,11 @@ public class BabelCodegen {
         final String xml = out.toString()
                 .replaceAll("<(\\w+:)?configuration\\s+xmlns(:\\w+)?=\"[^\"]*\"[^>]*>", "<$1configuration xmlns$2=\"" + REPO_BABEL_CODEGEN_XSD + "\">")
                 .replace("<configuration>", "<configuration xmlns=\"" + REPO_BABEL_CODEGEN_XSD + "\">");
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try {
             final SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
@@ -122,14 +189,18 @@ public class BabelCodegen {
     }
 
     public void generate(File sourceDir, String subDir, Source source, BabelOptions babelOptions) {
+        if (!sourceDir.exists())
+            sourceDir = new File(sourceDir.getAbsolutePath());
+
         final File[] childFiles = sourceDir.listFiles();
         if (childFiles == null)
             return;
 
         final String dir = source.getTargetDir();
-        final File targetDir = new File(dir
+        final File targetDir = new File(new File(dir
                 + (dir.length() > 0 && dir.length() - 1 != dir.lastIndexOf(File.separatorChar) ? File.separatorChar : "")
-                + sourceDir.getName());
+                + subDir).getAbsolutePath());
+
         if (!targetDir.mkdirs() && !targetDir.isDirectory()) {
             System.err.println(targetDir.getAbsolutePath() + " is not directory.");
             return;
@@ -137,26 +208,31 @@ public class BabelCodegen {
 
         for (File file : childFiles) {
             if (file.isDirectory()) {
-                generate(file, subDir + File.separatorChar + sourceDir.getName(), source, babelOptions);
+                if (source.isRecursive())
+                    generate(file, subDir + (subDir.length() > 0 ? File.separatorChar : "") + file.getName(), source, babelOptions);
                 continue;
             }
 
-            final String fileName = file.getName();
-            final String postfix = source.getSourceFilePostfix();
-            if (fileName.length() <= postfix.length() || fileName.lastIndexOf(postfix) != fileName.length() - postfix.length())
+            if (file.exists() && !source.isOverwrite())
                 continue;
 
-            final File targetFile = new File(targetDir, fileName.substring(0, fileName.lastIndexOf(postfix)) + ".js");
+            final String fileName = file.getName();
+            final String postfix = source.getSourceFilePostfix();
+            if (fileName.length() <= postfix.length() || fileName.toLowerCase().lastIndexOf(postfix) != fileName.length() - postfix.length())
+                continue;
+
+            final File targetFile = new File(targetDir, fileName.substring(0, fileName.toLowerCase().lastIndexOf(postfix)) + ".js");
             if (targetFile.exists())
                 if (!source.isOverwrite() || targetFile.isDirectory())
                     continue;
 
             try {
                 final String originCode = new String(Files.readAllBytes(Paths.get(file.toURI())), source.getSourceEncoding());
-                final ScriptObjectMirror babelResultMirror = (ScriptObjectMirror) invocable.invokeFunction("transform", originCode, babelOptions.getPresets(), babelOptions.isMinified());
-                final String transformCode = (String) babelResultMirror.getMember("code");
-                Files.write(Paths.get(targetFile.toURI()), transformCode.getBytes(source.getTargetEncoding()));
-            } catch (IOException | NoSuchMethodException | ScriptException e) {
+                bindings.put("input", originCode);
+                final String output = (String) engine.eval("Babel.transform(input, { presets: ['" + babelOptions.getPresets().value() + "'], minified: " + babelOptions.isMinified() + " }).code", bindings);
+                Files.write(Paths.get(targetFile.toURI()), output.getBytes(source.getTargetEncoding()));
+                System.out.println("   transformed: " + file.getAbsolutePath() + " -> " + targetFile.getAbsolutePath());
+            } catch (IOException | ScriptException e) {
                 e.printStackTrace();
             }
         }
